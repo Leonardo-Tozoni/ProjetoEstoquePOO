ba822f06b86baf42dc855cbe8a2e57403b0b266b
Leonardo Luís Tozoni <47570204+Leonardo-Tozoni@users.noreply.github.com>
2022-09-10 09:53:11 -0300

    Criar classes e gerenciamento de opções com switch

---
+++ ProjetoEstoquePOO/src/cli/GerenciarEstoque.java
@@ -0,0 +1,55 @@
+package cli;
+
+import model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GerenciarEstoque {
+    public List<Product> listProduct = new ArrayList<>();
+
+    public static void main(String[] args) {
+        GerenciarEstoque gerenciar =new GerenciarEstoque();
+        Scanner scan = new Scanner(System.in);
+        int option   = 0;
+        do{
+            option = construirMenu(gerenciar, scan);
+        }while(option!=9);
+    }
+
+    private static int construirMenu(GerenciarEstoque gerenciar, Scanner scan) {
+        int option;
+        System.out.println("---------MENU-----------");
+        System.out.println("1. Cadastro de Novo Produto");
+        System.out.println("2. Repor Estoque");
+        System.out.println("3. Retirar Estoque");
+        System.out.println("4. Pesquisar Produto");
+        System.out.println("5. Relatório de Produtos Cadastrados");
+        System.out.println("9. Sair");
+        System.out.println("Digite uma opção: ");
+        option = Integer.parseInt(scan.nextLine());
+        switch (option) {
+            case 1:
+                gerenciar.execRegistrarProduto(scan);
+                break;
+            case 2:
+                gerenciar.execAddEstoque(scan);
+                break;
+            case 3:
+                gerenciar.execRemoverEstoque(scan);
+                break;
+            case 4:
+                gerenciar.execMostrarProduto(scan);
+                break;
+            case 5:
+                gerenciar.execMostrarTodo();
+                break;
+            case 9:
+                System.out.println("FIM");
+                break;
+            default:
+                System.out.println("Opção inválida");
+        }
+        return option;
+    }
---
+++ ProjetoEstoquePOO/src/model/Produto.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Produto {
+    private int sku;
+    private String nomeProduto;
+    private double quantidadeProduto;
+    private double precoProduto;
+
+    public int getCodigo() {
+        return sku;
+    }
+    public void setCodigo(int sku) {
+        this.sku = sku;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+
+    public double getQuantidadeProduto() {
+        return quantidadeProduto;
+    }
+    public void setQuantidadeProduto(double quantidadeProduto) {
+        this.quantidadeProduto = quantidadeProduto;
+    }
+
+    public double getPrecoProduto() {
+        return precoProduto;
+    }
+
+    public void setPrecoProduto(double precoProduto) {
+        this.precoProduto = precoProduto;
+    }
